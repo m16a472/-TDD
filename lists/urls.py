4af71edf293b20fdec3f2cdd37cf329f4015cd26
m16a472 <2249780030@qq.com>
2022-06-07 15:08:11 +0800

    real last commit

--- lists/urls.py
+++ lists/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 #from django.contrib import admin
 from django.urls import path, re_path
 from lists import views
-
 urlpatterns = [
     path('lists/new',views.new_list,name='new_list'),
     re_path(r'^lists/(\d+)/$',views.view_list, name='view_list'),
--- suplerlists/urls.py
+++ suplerlists/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 #from django.contrib import admin
-from django.urls import path, re_path
-from lists import views
+from django.urls import path,re_path,include
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
     path('', list_views.home_page,name='home'),
