66959ce562458b46d445067aa3ce2c08a8a5564a
m16a472 <2249780030@qq.com>
2022-06-07 14:54:02 +0800

    last commit 3

--- lists/urls.py
+++ lists/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, re_path
 from lists import views
 
 urlpatterns = [
-    path('', views.home_page,name='home'),
     path('lists/new',views.new_list,name='new_list'),
     re_path(r'^lists/(\d+)/$',views.view_list, name='view_list'),
     re_path(r'^lists/(\d+)/add_item$',views.add_item, name='add_item'),
